fa01b88e68d79a67a333393f7a05f4fb3a297c65
Igor <nekto.korobko@gmail.com>
2022-07-10 22:51:49 +0300

    Create function for save data to postgres

---
+++ 03_sqlite_to_postgres/postgres_saver.py
@@ -0,0 +1,26 @@
+from dataclasses import astuple, fields
+from logging import getLogger
+
+import psycopg2
+from psycopg2.extras import execute_values
+
+
+class PostgresSaver:
+    def __init__(self, pg_conn: psycopg2.extensions.connection):
+        self._connection = pg_conn
+        self._cursor = self._connection.cursor()
+        self._logger = getLogger()
+
+    def save_all_data(self, talbe_name, data, dataclass):
+        """Метод сохранения данных в таблицу PostgreSQL"""
+        try:
+            print('fff')
+            keys = ", ".join(field.name for field in fields(dataclass))
+            execute_values(
+                self._cursor,
+                f"""INSERT INTO content.{talbe_name} ({keys})
+                VALUES %s ON CONFLICT (id) DO NOTHING;""",
+                [astuple(row) for row in data])
+            print('qqqq')
+        except Exception as exception:
+            self._logger.error(exception)
